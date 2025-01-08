198c0d8b67a68ab3a0a789c7ca07d4921f65ddb9
Nathan Dejesus <100551611+AvalonAce@users.noreply.github.com>
2025-01-07 20:38:03 -0500

    main config update

--- main_config.py
+++ main_config.py
@@ -1,10 +1,16 @@
-OPEN_AI_API_KEY = "KEY_HERE"
-ELEVENLABS_API_KEY = "KEY_HERE"
+import yaml
 
-USER_NAME = "Boss"
+# Load the configuration for main
+with open("./config/main_config.yaml", "r") as file:
+  main_config = yaml.safe_load(file)
+
+# Load Variables
+OPEN_AI_API_KEY = main_config["Keys"]["OPEN_AI_API_KEY"]
+ELEVENLABS_API_KEY = main_config["Keys"]["ELEVENLABS_API_KEY"]
+USER_NAME = main_config["User"]["USER_NAME"]
 
 BUILD_SETTINGS = {
-  "systemVoice" : True,
-  "starterVoice": "Taron",
-  "currentVoice": "Taron",
+  "systemVoice" : main_config["Voices"]["useSystemVoice"],
+  "starterVoice": main_config["Voices"]["starterVoice"],
+  "currentVoice": main_config["Voices"]["currentVoice"],
 }
