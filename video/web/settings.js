4f000695e1a90070de6ce1a8291071cc99e39733
HeatherCasto <heathercasto101@gmail.com>
2025-02-18 17:06:44 -0500

    login sign up page design

--- video/web/settings.js
+++ video/web/settings.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const savedTheme = localStorage.getItem('theme');
+    const settingsExitIcon = document.querySelector(".settings-exit");
     if (savedTheme === 'dark') {
         document.body.classList.add('dark-mode');
+        updateExitIcon();
     }
 });
 
@@ -9,3 +11,10 @@ document.querySelector(".settings-exit").addEventListener("click", function() {
     window.location.href = "index.html"; // Redirect to index page
 });
 
+function updateExitIcon() {
+    if (document.body.classList.contains("dark-mode")) {
+        settingsExitIcon.src = "./Images/darkX.png";
+    } else {
+        settingsExitIcon.src = "./Images/lightX.png";
+    }
+}
